f813584f1daff2497490500b5d17b161c0a38967
Mustapha Issa Toyin <31075233+emaitee@users.noreply.github.com>
2022-10-12 17:44:14 +0100

    Rearrange models

--- src/Login.js
+++ src/Login.js
@@ -1,58 +1,77 @@
-import React from "react";
-import {  Button, Card, CardBody, CardHeader, Col,  FormGroup,  Input, Label, Row, Spinner } from "reactstrap";
-import bg from "./img/bg.jpg";
+import React from 'react'
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+  Spinner,
+} from 'reactstrap'
+import bg from './img/bg.jpg'
 import SelectInput from './components/UI/SelectInput'
-import "./Signup.css";
-import { useNavigate } from "react-router";
+import './Signup.css'
+import { useNavigate } from 'react-router'
 
-
-function Signup () {
+function Signup() {
   const navigate = useNavigate()
 
   return (
-    
-     <div
-        className="m-0"
+    <div
+      className="m-0"
+      style={{
+        backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7) ),url(${bg})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <h1
         style={{
-          backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7) ),url(${bg})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          height: "100vh",
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center'
+          color: 'white',
+          fontFamily: 'font-family: "Gill Sans", sans-serif;',
         }}
       >
-     <h1 style={{color:'white',fontFamily:'font-family: "Gill Sans", sans-serif;'}}>laLoona</h1>
-     {/* <Button>Login</Button> */}
-    
-     <Row>
-        <Col className="offset-md-2" md={8} >
-          <Card style={{borderRadius: '0',border:'none', backgroundColor: 'rgba(127, 205, 218, 0.1)',color:'white',fontFamily:'font-family: "Gill Sans", sans-serif;',}}>
-              <CardHeader style={{}}>Login</CardHeader>
-              <CardBody> 
-              <Row className="login-row mt-2">  
-              
-              {/* <div className="my-2"> 
+        laLoona
+      </h1>
+      {/* <Button>Login</Button> */}
+
+      <Row>
+        <Col className="offset-md-2" md={8}>
+          <Card
+            style={{
+              borderRadius: '0',
+              border: 'none',
+              backgroundColor: 'rgba(127, 205, 218, 0.1)',
+              color: 'white',
+              fontFamily: 'font-family: "Gill Sans", sans-serif;',
+            }}
+          >
+            <CardHeader style={{}}>Login</CardHeader>
+            <CardBody>
+              <Row className="login-row mt-2">
+                {/* <div className="my-2"> 
               <p>Name</p>
           <Input
           className="form-control"
           placeholder="e.g. John Doe" required
           />
         </div> */}
-            
-      
-        <div  className="my-2"> 
-        <p>Email</p>
-            <Input
-          type="email" required
-          placeholder="example@mail.com"
-          />
-          </div>
-    
-          {/* <Col md={4} > 
+
+                <div className="my-2">
+                  <p>Email</p>
+                  <Input type="email" required placeholder="example@mail.com" />
+                </div>
+
+                {/* <Col md={4} > 
           <p>Age</p>
             <Input
           type="number"
@@ -60,21 +79,44 @@ function Signup () {
           placeholder="Age"
           />
           </Col> */}
-          <div className="my-2"> 
-          <p>Password</p>
-        <Input
-          type="password"
-          placeholder="*******" required  
-          />
-          </div>
+                <div className="my-2">
+                  <p>Password</p>
+                  <Input type="password" placeholder="*******" required />
+                </div>
 
-          <div className="my-2"> 
-          
-        <Button style={{width:'100%',backgroundColor:'rgb(35, 13, 54)',border:'none'}}   className="mt-2">Continue witch Google</Button>
-        <Button style={{width:'100%', backgroundColor:'rgb(35, 13, 54)',border:'none'}}   className="mt-2">Continue witch Facebook</Button>
-        <Button style={{width:'100%', backgroundColor:'rgb(35, 13, 54)',border:'none'}}   className="mt-2">Continue witch Instagram</Button>
-          </div>
-          {/* <Col md={4} > 
+                <div className="my-2">
+                  <Button
+                    style={{
+                      width: '100%',
+                      backgroundColor: 'rgb(35, 13, 54)',
+                      border: 'none',
+                    }}
+                    className="mt-2"
+                  >
+                    Continue witch Google
+                  </Button>
+                  <Button
+                    style={{
+                      width: '100%',
+                      backgroundColor: 'rgb(35, 13, 54)',
+                      border: 'none',
+                    }}
+                    className="mt-2"
+                  >
+                    Continue witch Facebook
+                  </Button>
+                  <Button
+                    style={{
+                      width: '100%',
+                      backgroundColor: 'rgb(35, 13, 54)',
+                      border: 'none',
+                    }}
+                    className="mt-2"
+                  >
+                    Continue witch Instagram
+                  </Button>
+                </div>
+                {/* <Col md={4} > 
       <p>Gender</p>
                     <select
                       className="form-control"
@@ -85,31 +127,30 @@ function Signup () {
                     </select>
       </Col> */}
 
-        
-          {/* <Col md={4} > 
+                {/* <Col md={4} > 
           <p>Height</p>
             <Input
           type="number"
           placeholder="height"
           />
       </Col> */}
-          
-      {/* <Col md={4} > 
+
+                {/* <Col md={4} > 
       <p>Hips</p>
             <Input
           type="number"
           placeholder="hips"
           />
       </Col> */}
-      {/* <Col md={4} > 
+                {/* <Col md={4} > 
       <p>Bust</p>
             <Input
           type="number"
           placeholder="bust"
           />
       </Col> */}
-      
-      {/* <Col md={4}>
+
+                {/* <Col md={4}>
                     <p>Complextion</p>
                     <select
                       className="form-control"
@@ -124,8 +165,8 @@ function Signup () {
                       <option>dark brown</option>
                     </select>
                   </Col> */}
-      
-                  {/* <Col md={4}>
+
+                {/* <Col md={4}>
                     <p>Shape</p>
                     <select
                       className="form-control"
@@ -140,7 +181,7 @@ function Signup () {
                       <option>ghjk</option>
                     </select>
                   </Col> */}
-                  {/* <Col md={4}>
+                {/* <Col md={4}>
                       <p>Location</p>
                 
                         <select
@@ -157,8 +198,8 @@ function Signup () {
                     </select>
                   
         </Col> */}
-      
-        {/* <Col md={4} > 
+
+                {/* <Col md={4} > 
       <p>Bio</p>
             <Input
           type="textarea"
@@ -166,23 +207,24 @@ function Signup () {
           />
       </Col> */}
 
-      <Col md={{ size: '6', offset: '9' }}>
-                    <Button
-                      className="mt-2"
-                      color="success"
-                      onClick={() => navigate('/GeneralModels')}
-                      style={{backgroundColor:'rgb(35, 13, 54)',border:"none",width:"50%"}}
-
-                    >
-                    
-                        <Spinner color="light" size={'sm'} className="me-1">
-                          Loading...
-                        </Spinner>
-                    
-                      Submit
-                    </Button>
-                  </Col>
-                  {/* <div className="text-center">
+                <Col md={{ size: '6', offset: '9' }}>
+                  <Button
+                    className="mt-2"
+                    color="success"
+                    onClick={() => navigate('/general-models')}
+                    style={{
+                      backgroundColor: 'rgb(35, 13, 54)',
+                      border: 'none',
+                      width: '50%',
+                    }}
+                  >
+                    <Spinner color="light" size={'sm'} className="me-1">
+                      Loading...
+                    </Spinner>
+                    Submit
+                  </Button>
+                </Col>
+                {/* <div className="text-center">
                           <hr style={{ padding: 0 }}></hr>
                           <p
                             style={{
@@ -198,17 +240,13 @@ function Signup () {
                             </span>{" "}
                           </p>
                         </div> */}
-                  </Row>
-      
-              </CardBody>
-      
+              </Row>
+            </CardBody>
           </Card>
         </Col>
-     </Row>
-     
-      </div>
-    
-  );
+      </Row>
+    </div>
+  )
 }
 
-export default Signup;
+export default Signup
--- src/components/routes/AppNavigation.js
+++ src/components/routes/AppNavigation.js
@@ -1,34 +1,29 @@
-import React from 'react';
-import { useRoutes } from 'react-router';
-import Signup from '../../Signup';
+import React from 'react'
+import { useRoutes } from 'react-router'
+import Signup from '../../Signup'
 // import MenuIndex from '../../Pages/Sidebar/MenuIndex';
 // import Welcome from '../../Pages/Welcome';
-import Login from '../../Login';
-import GeneralModels from '../../GeneralModels';
-import Model from '../../Model';
-
-
-
-
+import Login from '../../Login'
+import GeneralModels from '../../GeneralModels'
+import Model from '../../Model'
 
 function AppNavigation() {
   let element = useRoutes([
     {
       path: '/',
-      
+
       element: <Signup />,
     },
     {
       path: '/Login',
       element: <Login />,
-
     },
     {
-      path: '/GeneralModels',
+      path: '/general-models',
       element: <GeneralModels />,
     },
-  ]);
-  return element;
+  ])
+  return element
 }
 
-export default AppNavigation;
+export default AppNavigation
