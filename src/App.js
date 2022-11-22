c443bf4ccd796f184a793a134c6c28a926d0ee9c
Mustapha Issa Toyin <31075233+emaitee@users.noreply.github.com>
2022-11-22 11:58:18 +0100

    xee

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React from "react";
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AppNavigation from "./components/routes/AppNavigation";
-import { BrowserRouter } from "react-router-dom";
-import Navbar from "./Navbar";
-
+import AppNavigation from './components/routes/AppNavigation';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter >
-      <Navbar expand={'md'} dark color='dark' />
-     <AppNavigation/>
-    
+    <BrowserRouter>
+      <AppNavigation />
     </BrowserRouter>
   );
 }
